be73371c643539810174334bbc44a9c0a3e1e86a
mark-smirnov <mark-smirnov@yandex-team.ru>
2020-04-18 22:55:58 +0300

    eventPanel: ребейз

--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -10,26 +10,26 @@ import { getParsedTime } from '../../utils/getParsedTime';
 import './PreviewCard.css'
 
 const PreviewCard = ({ id, className, name, date, type, onPreviewCardClick }) => {
-  const { time, ...moments } = useTimer(date);
-  const [amount, dateType] = getParsedTime(moments);
+    const { time, ...moments } = useTimer(date);
+    const [amount, dateType] = getParsedTime(moments);
 
-  return (
-    <Div className='preview-card-wrapper' data-to='event' onClick={(e) => onPreviewCardClick(e, id)}>
-      <div className={cn(['preview-card', className, `preview-card-${type}`])}>
-        <div className='card-event'>
-          <Text color='transparent' font='body3'>{time === 'future' ? 'До' : 'C'}</Text>
-          <Text color='white' font='h3' bold>{name}</Text>
-        </div>
-        <div className='card-date'>
-          <Text color='transparent' font='body3'>{getReadableDate(date)}</Text>
-            <div className='date-inline'>
-                <Text color='white' font='h3' bold>{amount}</Text>
-                <Text className='date-inline-type' color='transparent' font='body3'>{dateType}</Text>
-            </div>
-        </div>
-      </div>
-    </Div>
-  );
+    return (
+        <Div className='preview-card-wrapper' data-to='event' onClick={(e) => onPreviewCardClick(e, id)}>
+              <div className={cn(['preview-card', className, `preview-card-${type}`])}>
+                    <div className='card-event'>
+                          <Text color='transparent' font='body3'>{time === 'future' ? 'До' : 'C'}</Text>
+                          <Text color='white' font='h3' bold>{name}</Text>
+                    </div>
+                    <div className='card-date'>
+                          <Text color='transparent' font='body3'>{getReadableDate(date)}</Text>
+                                <div className='date-inline'>
+                                    <Text color='white' font='h3' bold>{amount}</Text>
+                                    <Text className='date-inline-type' color='transparent' font='body3'>{dateType}</Text>
+                                </div>
+                    </div>
+          </div>
+        </Div>
+    );
 };
 
 export default PreviewCard
