160d7491036ce96fd216b997e2f6afb5dc7cd3b2
mark-smirnov <mark-smirnov@yandex-team.ru>
2020-05-05 14:44:32 +0300

    token_security: правки по линту

--- src/store/actions/events.js
+++ src/store/actions/events.js
@@ -2,59 +2,68 @@ export const LoadingState = {
 	NotLoaded: 0,
 	Loading: 1,
 	Loaded: 2,
-	Errored: 3,
+	Errored: 3
 };
 
 export const EventsActionTypes = {
 	SET_EVENTS: 'EVENTS:SET_EVENTS',
 	SET_LOADING_STATE: 'EVENTS:SET_LOADING_STATE',
-	ADD_EVENT: 'EVENTS:ADD_EVENT',
+	ADD_EVENT: 'EVENTS:ADD_EVENT'
 };
 
 export const setEvents = events => ({
 	type: EventsActionTypes.SET_EVENTS,
 	payload: {
-		events,
-	},
+		events
+	}
 });
 
 export const addEvent = event => ({
 	type: EventsActionTypes.ADD_EVENT,
 	payload: {
-		event,
-	},
+		event
+	}
 });
 
 export const setEventsLoadingState = loadingState => ({
 	type: EventsActionTypes.SET_LOADING_STATE,
 	payload: {
-		loadingState,
-	},
+		loadingState
+	}
 });
 
 export const loadEvents = id => dispatch => {
-    dispatch(setEventsLoadingState(LoadingState.Loading));
+	dispatch(setEventsLoadingState(LoadingState.Loading));
 
-    fetch(`${process.env.REACT_APP_LOCAL_URL || process.env.REACT_APP_PUBLIC_URL}/user/${id}/${process.env.REACT_APP_API_TOKEN}`)
-        .then(res => res.json())
-        .then(({ events }) => {
-            dispatch(setEvents(events));
-            dispatch(setEventsLoadingState(LoadingState.Loaded));
-        })
-        .catch(err => {
-            console.log(err);
-            dispatch(setEventsLoadingState(LoadingState.Errored));
-        });
+	fetch(
+		`${process.env.REACT_APP_LOCAL_URL || process.env.REACT_APP_PUBLIC_URL}/user/${id}/${
+			process.env.REACT_APP_API_TOKEN
+		}`
+	)
+		.then(res => res.json())
+		.then(({ events }) => {
+			dispatch(setEvents(events));
+			dispatch(setEventsLoadingState(LoadingState.Loaded));
+		})
+		.catch(err => {
+			console.log(err);
+			dispatch(setEventsLoadingState(LoadingState.Errored));
+		});
 };
 
 export const createEvent = (id, event) => dispatch => {
-    dispatch(addEvent(event));
+	dispatch(addEvent(event));
 
-    fetch(`${process.env.REACT_APP_LOCAL_URL || process.env.REACT_APP_PUBLIC_URL}/user/${id}/${process.env.REACT_APP_API_TOKEN}/add_event`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ event }),
-    })
+	fetch(
+		`${process.env.REACT_APP_LOCAL_URL || process.env.REACT_APP_PUBLIC_URL}/user/${id}/${
+			process.env.REACT_APP_API_TOKEN
+		}/add_event`,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ event })
+		}
+	);
 };
