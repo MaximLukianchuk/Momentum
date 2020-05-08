e30818671536cededb59acfd4a943d850b90b9f0
MaximLukianchuk <44572418+MaximLukianchuk@users.noreply.github.com>
2020-05-08 22:12:31 +0000

    Deploying to gh-pages from  @ ac4ff90b9ce0a67f9a7906e48d64553eecd6ff2e 🚀

---
+++ precache-manifest.382e9df814f49b874af6a24120f9416c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0a297fe175d0469f47ff5ac0c41403",
+    "url": "/Momentum/index.html"
+  },
+  {
+    "revision": "f404759a68cacc2de708",
+    "url": "/Momentum/static/css/2.47f050ad.chunk.css"
+  },
+  {
+    "revision": "80beb836846ac31984d4",
+    "url": "/Momentum/static/css/main.5ddfe944.chunk.css"
+  },
+  {
+    "revision": "f404759a68cacc2de708",
+    "url": "/Momentum/static/js/2.51e2b2e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Momentum/static/js/2.51e2b2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80beb836846ac31984d4",
+    "url": "/Momentum/static/js/main.729d2b62.chunk.js"
+  },
+  {
+    "revision": "f560214a3ad47f08dae8",
+    "url": "/Momentum/static/js/runtime-main.cd8320be.js"
+  },
+  {
+    "revision": "8d4b57d8ec23f4f5f7a315bca60a563b",
+    "url": "/Momentum/static/media/blue-gradient-example.8d4b57d8.png"
+  },
+  {
+    "revision": "aec9037540764d4e127d09112a5319a0",
+    "url": "/Momentum/static/media/blue-gradient.aec90375.jpg"
+  },
+  {
+    "revision": "4399b6c5a6316802e391ba8967ee8933",
+    "url": "/Momentum/static/media/red-gradient-example.4399b6c5.png"
+  },
+  {
+    "revision": "e5917570dc087e1b79552a75f835f5f4",
+    "url": "/Momentum/static/media/red-gradient.e5917570.jpg"
+  },
+  {
+    "revision": "a57be422e02e768d9ab2f679fff6ff17",
+    "url": "/Momentum/static/media/violet-gradient-example.a57be422.png"
+  },
+  {
+    "revision": "16e7ac8edc6a1d38d2f508f1008512e9",
+    "url": "/Momentum/static/media/violet-gradient.16e7ac8e.jpg"
+  }
+]);
